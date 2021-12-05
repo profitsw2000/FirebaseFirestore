2ae3e84aee284b003b99455d703922faad9e38d9
profitsw2000 <profitsw2000@gmail.com>
2021-12-05 20:52:31 +0300

    Make application write FC info to realtime database by adding to code of InputFormFragment.java button listener method. It get text from edit elements and send it to database.

--- app/src/main/java/ru/profitsw2000/firebasefirestore/InputFormFragment.java
+++ app/src/main/java/ru/profitsw2000/firebasefirestore/InputFormFragment.java
@@ -7,13 +7,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Fragment with form used for adding info to database.
  */
 public class InputFormFragment extends Fragment {
 
-    View rootView   ;
+    private String KEY = "footbal_club" ;
+
+    private View rootView   ;
+    private Button addButton    ;
+    private EditText clubEdit, cityEdit, countryEdit    ;
+    private DatabaseReference databaseReference  ;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +39,37 @@ public class InputFormFragment extends Fragment {
         return rootView ;
     }
 
+    /**
+     * Method for initialization fragment UI.
+     */
     private void initUI(){
+        addButton = rootView.findViewById(R.id.button_add_club_info)    ;
+        clubEdit = rootView.findViewById(R.id.editClub) ;
+        cityEdit = rootView.findViewById(R.id.editCity) ;
+        countryEdit = rootView.findViewById(R.id.editCountry)   ;
 
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance("https://fir-firestoreproject-6374c-default-rtdb.europe-west1.firebasedatabase.app/")   ;
+        databaseReference = firebaseDatabase.getReference(KEY)  ;
+
+        //click on button ADD NEW CLUB
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //get id and club info from form
+                String id = databaseReference.getKey();
+                String club = clubEdit.getText().toString() ;
+                String city = cityEdit.getText().toString() ;
+                String country = countryEdit.getText().toString() ;
+
+                //create new instance of FC class and write it to realtime database
+                FootballClub footballClub = new FootballClub(id, club, city, country)   ;
+                databaseReference.push().setValue(footballClub) ;
+
+                //clear form
+                clubEdit.getText().clear();
+                cityEdit.getText().clear();
+                countryEdit.getText().clear();
+            }
+        });
     }
 }

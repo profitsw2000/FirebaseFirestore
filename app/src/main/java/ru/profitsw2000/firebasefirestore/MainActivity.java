9ae88f1f837926f74061746b537e6b20b6d3d438
profitsw2000 <profitsw2000@gmail.com>
2021-12-05 09:33:49 +0300

    Make database work. Write simple code that send string to database while created main activity.

--- app/src/main/java/ru/profitsw2000/firebasefirestore/MainActivity.java
+++ app/src/main/java/ru/profitsw2000/firebasefirestore/MainActivity.java
@@ -1,14 +1,32 @@
 package ru.profitsw2000.firebasefirestore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "Logging"  ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Write a message to the database
+        FirebaseDatabase mDatabase = FirebaseDatabase.getInstance("https://fir-firestoreproject-6374c-default-rtdb.europe-west1.firebasedatabase.app/");
+        DatabaseReference mDbRef = mDatabase.getReference("Donor/Name");
+        mDbRef.setValue("Parinitha");
     }
 }

701a1865346514bb69593b09b9bcf8214d2584b6
profitsw2000 <profitsw2000@gmail.com>
2021-12-05 16:37:36 +0300

    Set listener to write button. After it click starts fragment with form.

--- app/src/main/java/ru/profitsw2000/firebasefirestore/MainActivity.java
+++ app/src/main/java/ru/profitsw2000/firebasefirestore/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    Navigation navigation   ;
+    public Navigation navigation   ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/ru/profitsw2000/firebasefirestore/StartFragment.java
+++ app/src/main/java/ru/profitsw2000/firebasefirestore/StartFragment.java
@@ -3,10 +3,12 @@ package ru.profitsw2000.firebasefirestore;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Fragment to start with application. It contain two buttons to select
@@ -16,6 +18,8 @@ public class StartFragment extends Fragment {
 
     private View rootView   ;
     private Navigation navigation   ;
+    Button writeButton, readButton  ;
+    FragmentManager fragmentManager ;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,6 +36,17 @@ public class StartFragment extends Fragment {
     }
 
     private void initUI() {
+        writeButton = (Button) rootView.findViewById(R.id.button_write) ;
 
+        navigation = new Navigation(getActivity().getSupportFragmentManager())  ;
+
+        writeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InputFormFragment inputFormFragment = new InputFormFragment()   ;
+
+                navigation.addFragment(inputFormFragment, R.id.main_frame, true);
+            }
+        });
     }
 }

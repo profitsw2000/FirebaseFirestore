b5ba9d2aafbe6c4237f6a02da43cb8adbe642c76
profitsw2000 <profitsw2000@gmail.com>
2021-12-05 15:59:15 +0300

    Created new fragment to start with application. It contain two buttons to select either to write info to Firebase or read from it. Make layout filled with two buttons.

---
+++ app/src/main/java/ru/profitsw2000/firebasefirestore/StartFragment.java
@@ -0,0 +1,36 @@
+package ru.profitsw2000.firebasefirestore;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Fragment to start with application. It contain two buttons to select
+ * either to write info to Firebase or read from it.
+ */
+public class StartFragment extends Fragment {
+
+    View rootView   ;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        rootView = inflater.inflate(R.layout.fragment_start, container, false);
+        initUI()    ;
+        return rootView ;
+    }
+
+    private void initUI() {
+
+    }
+}
